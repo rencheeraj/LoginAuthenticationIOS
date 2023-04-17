b42a6d6a1ccbfc23430efe013b2df398ae676983
rencheeraj.mohan <>
2023-04-17 16:20:28 +0530

    Alert View

--- LoginPage/LoginPage/Views/ViewController.swift
+++ LoginPage/LoginPage/Views/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
     @IBOutlet weak var usernameText: UITextField!
@@ -15,28 +14,42 @@ class ViewController: UIViewController {
     
     private var token : String?
     @IBAction func loginAction(_ sender: Any) {
-//        guard let user = usernameText.text else {return}
-//        guard let password = passwordText.text else {return}
-        let user = "mtestuser"
-        let password = "mtest_spark"
-        let modelLogin = LoginModel(username:user , password: password)
-        
-        APICaller.shared.loginApiCalling(login: modelLogin){(result) in
-            switch result{
-            case .success(let json):
-                print(json)
-                self.token = (json as! ResponseModel).access
-                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-                let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ImageListViewController") as! ImageListViewController
-                nextViewController.modalPresentationStyle = .fullScreen
-                nextViewController.token = self.token
-                self.present(nextViewController, animated:true, completion:nil)
-            case .failure(let err):
-                print(err.localizedDescription )
-                
-            }
+        guard let user = usernameText.text else {return}
+        guard let password = passwordText.text else {return}
+            //        let user = "mtestuser"
+            //        let password = "mtest_spark"
+            let modelLogin = LoginModel(username:user , password: password)
+            APICaller.shared.loginApiCalling(login: modelLogin){(result) in
+                switch result{
+                case .success(let json):
+                    print(json)
+                    self.token = (json as! ResponseModel).access
+                    self.showNextView(tc: self.token)
+                case .failure(let err):
+                    print(err.localizedDescription )
+                    self.alertShow(re: "AuthenticationFailed")
+                    
+                }
         }
         
+    }
+    func showNextView(tc : String?){
+        DispatchQueue.main.async {
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ImageListViewController") as! ImageListViewController
+            nextViewController.modalPresentationStyle = .fullScreen
+            nextViewController.token = tc
+            self.present(nextViewController, animated:true, completion:nil)
+            self.usernameText.text = ""
+            self.passwordText.text = ""
+        }
+    }
+    func alertShow(re : String?){
+            DispatchQueue.main.async {
+                let alert = UIAlertController(title: "Access Denied", message: "Username or Password Incorrect", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+                self.present(alert, animated: true)
+            }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
